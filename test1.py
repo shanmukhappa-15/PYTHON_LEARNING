87cc71f21220c360b0690a2119739a3accc5f47d
shanmukhappa-15 <anupisnull@gmail.com>
2026-04-23 11:27:38 +0530

    test1.py is added

---
+++ test1.py
@@ -0,0 +1,26 @@
+# 1) prints A
+
+# 2) 
+
+def question2():
+    
+    num = int(input("Enter number: "))
+
+    if ((num % 2) == 0):
+        print("Even")
+    else:
+        print("Odd")
+
+question2()
+
+# 3) 0 1 2
+
+# 4) 
+
+def question4():
+    
+    num = int(input("Enter a number?\n-->"))
+    for i in range(1,num+1):
+        print(i)
+
+# question4()
