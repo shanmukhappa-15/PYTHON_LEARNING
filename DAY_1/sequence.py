823c6e25a89689011ed0888f35bdf63755b3fb06
shanmukhappa-15 <anupisnull@gmail.com>
2026-04-23 11:23:44 +0530

    day1 added

---
+++ DAY_1/sequence.py
@@ -0,0 +1,64 @@
+def sequence():
+    print("Hello")
+    print("My name is Jarvis")
+    print("I am learning Python")
+
+# sequence()
+
+def Battery():
+    battery = 20
+
+    if battery < 30:
+        print("Low battery")
+
+# Battery()
+    
+def no_input():
+    number = int(input("Enter a number: "))
+
+    if number > 10:
+        print("Big")
+    else:
+        print("Small")
+
+# no_input()
+
+def charging():
+    for i in range(3):
+        print("Charging")
+
+# charging()
+
+def numbers():
+    for i in range(1, 6):
+        print(i)
+        
+# numbers()
+
+def greetings():
+    name = input("Enter your name: ")
+    print("Hello " + name)
+
+# greetings()
+
+def program1():
+    print("System starting")
+    print("System ready")
+
+# program1()
+
+def program2():
+    age = int(input("Enter your age: "))
+    
+    if age < 18:
+        print("You are young")
+    else:
+        print("You are adult")
+
+# program2()
+    
+def program3():
+    for i in range (1, 6):
+        print(i)
+
+# program3()
